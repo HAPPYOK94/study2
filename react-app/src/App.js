10a614b490e8ff7e14d6578900d832246d260757
HAPPYOK94 <okj941220@gmail.com>
2022-12-14 02:03:08 +0900

    react

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,76 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { eventWrapper } from "@testing-library/user-event/dist/utils";
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
+function Header(props) {
+  return (
+    <header>
+      <h1>
+        <a
+          href='/'
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode();
+          }}>
+          {props.title}
+        </a>
+      </h1>
+    </header>
+  );
+}
 
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(
+      <li key={t.id}>
+        <a
+          id={t.id}
+          href={"/read/" + t.id}
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode(event.target.id);
+          }}>
+          {t.title}
+        </a>
+      </li>
+    );
+  }
+  return (
+    <nav>
+      <ol>{lis}</ol>
+    </nav>
+  );
+}
 function App() {
-  return <div className='App'>Hello react!</div>;
+  const topics = [
+    { id: 1, title: "html", body: "html is..." },
+    { id: 2, title: "css", body: "css is..." },
+    { id: 3, title: "js", body: "js is..." },
+  ];
+  return (
+    <div>
+      <Header
+        title='React'
+        onChangeMode={() => {
+          alert("Header");
+        }}></Header>
+      <Nav
+        topics={topics}
+        onChangeMode={(id) => {
+          alert(id);
+        }}></Nav>
+      <Article title='welcome' body='Hello,web'></Article>
+    </div>
+  );
 }
 
 export default App;

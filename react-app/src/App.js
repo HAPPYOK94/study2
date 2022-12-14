57f30203de2e7bf87ccc9ff9edec39004badca90
HAPPYOK94 <okj941220@gmail.com>
2022-12-14 22:35:54 +0900

    react component study

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,6 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { eventWrapper } from "@testing-library/user-event/dist/utils";
+import React from "react";
+import { useState } from "react";
+
 function Article(props) {
   return (
     <article>
@@ -25,7 +25,6 @@ function Header(props) {
     </header>
   );
 }
-
 function Nav(props) {
   const lis = [];
   for (let i = 0; i < props.topics.length; i++) {
@@ -37,7 +36,7 @@ function Nav(props) {
           href={"/read/" + t.id}
           onClick={(event) => {
             event.preventDefault();
-            props.onChangeMode(event.target.id);
+            props.onChangeMode(Number(event.target.id));
           }}>
           {t.title}
         </a>
@@ -51,24 +50,42 @@ function Nav(props) {
   );
 }
 function App() {
+  const [mode, setMode] = useState("WELCOME");
+  const [id, setId] = useState(null);
   const topics = [
-    { id: 1, title: "html", body: "html is..." },
-    { id: 2, title: "css", body: "css is..." },
-    { id: 3, title: "js", body: "js is..." },
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+    { id: 3, title: "javascript", body: "javascript is ..." },
   ];
+  let content = null;
+  if (mode === "WELCOME") {
+    content = <Article title='Welcome' body='Hello, WEB'></Article>;
+  } else if (mode === "READ") {
+    let title,
+      body = null;
+    for (let i = 0; i < topics.length; i++) {
+      console.log(topics[i].id, id);
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>;
+  }
   return (
     <div>
       <Header
-        title='React'
+        title='WEB'
         onChangeMode={() => {
-          alert("Header");
+          setMode("WELCOME");
         }}></Header>
       <Nav
         topics={topics}
-        onChangeMode={(id) => {
-          alert(id);
+        onChangeMode={(_id) => {
+          setMode("READ");
+          setId(_id);
         }}></Nav>
-      <Article title='welcome' body='Hello,web'></Article>
+      {content}
     </div>
   );
 }

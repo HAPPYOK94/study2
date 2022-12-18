d391a8a39bc522b084d5fb3e1c9daccac0e39f1b
HAPPYOK94 <okj941220@gmail.com>
2022-12-18 18:00:01 +0900

    lorem

--- js2/script.js
+++ js2/script.js
@@ -1,7 +1,11 @@
 const a = 10;
 const b = 20;
+const c = 20;
 if (a > b) {
-  console.log("a가 더 큽니다!");
+  console.log("a가 b보다 큽니다");
+} else if (b === c) {
+  console.log("b랑 c가 같네요?");
 } else {
-  console.log("b가 더 큽니다!");
+  console.log("여기는 언제 출력도기ㅔ 될까요?");
 }
+lolem;

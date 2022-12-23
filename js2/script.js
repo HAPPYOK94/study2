d48549dfdfb5cfd8d32ecb99014b3d7851d754b7
HAPPYOK94 <okj941220@gmail.com>
2022-12-23 17:29:25 +0900

    반복문 공부

--- js2/script.js
+++ js2/script.js
@@ -1,11 +1,6 @@
-const a = 10;
-const b = 20;
-const c = 20;
-if (a > b) {
-  console.log("a가 b보다 큽니다");
-} else if (b === c) {
-  console.log("b랑 c가 같네요?");
-} else {
-  console.log("여기는 언제 출력도기ㅔ 될까요?");
+for (let i = 0; i < 10; i++) {
+  if (i === 5) {
+    continue;
+  }
+  console.log(i);
 }
-lolem;

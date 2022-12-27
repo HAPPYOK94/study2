0dabe8a9323971495ecf75282358c67e51df8853
HAPPYOK94 <okj941220@gmail.com>
2022-12-28 02:19:08 +0900

    array study

--- js2/script.js
+++ js2/script.js
@@ -1,8 +1,6 @@
 const arr = [1, 2, 3, 4, 5];
-function plusTwo(number) {
-  return number + 2;
-}
-
-const newArr = Array.from(arr, plusTwo);
 console.log(arr);
-console.log(newArr);
+
+console.log(arr.shift());
+console.log(arr.shift());
+console.log(arr);

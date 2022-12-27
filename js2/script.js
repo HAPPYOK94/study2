fbf5bb75f7b56feece6a911fea4af99334ac9c72
HAPPYOK94 <okj941220@gmail.com>
2022-12-27 23:13:11 +0900

    dream

--- js2/script.js
+++ js2/script.js
@@ -1,4 +1,8 @@
 const arr = [1, 2, 3, 4, 5];
-const newArr = Array.from(arr);
+function plusTwo(number) {
+  return number + 2;
+}
+
+const newArr = Array.from(arr, plusTwo);
 console.log(arr);
 console.log(newArr);

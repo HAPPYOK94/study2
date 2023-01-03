77ee5b3c695071ea7a685444d41cbb7d7c58ee3e
HAPPYOK94 <okj941220@gmail.com>
2023-01-03 22:34:01 +0900

    배열

--- js2/script.js
+++ js2/script.js
@@ -1,2 +1,21 @@
-const arr = [1, 2, 3, 4, 5];
-const key = arr.findIndex((number) => number > 3);
+const users = [
+  {
+    name: "도엽",
+    point: 20,
+  },
+  {
+    name: "규옥",
+    point: 100,
+  },
+  {
+    name: "혜지",
+    point: 40,
+  },
+  {
+    name: "하영",
+    point: 30,
+  },
+];
+
+const reducer = (acc, obj) => acc + obj.point;
+console.log(users.reduce(reducer, 0));

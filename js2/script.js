e1c68e2f3a92b191a15ca33ad83aa7346d0cdfc4
HAPPYOK94 <okj941220@gmail.com>
2023-01-04 23:27:10 +0900

    ddd

--- js2/script.js
+++ js2/script.js
@@ -1,21 +1,9 @@
-const users = [
-  {
-    name: "도엽",
-    point: 20,
+const user = {
+  name: "규옥",
+  age: 30,
+  job: "joplees",
+  getName() {
+    console.log(this.name);
   },
-  {
-    name: "규옥",
-    point: 100,
-  },
-  {
-    name: "혜지",
-    point: 40,
-  },
-  {
-    name: "하영",
-    point: 30,
-  },
-];
-
-const reducer = (acc, obj) => acc + obj.point;
-console.log(users.reduce(reducer, 0));
+};
+user.getName();
